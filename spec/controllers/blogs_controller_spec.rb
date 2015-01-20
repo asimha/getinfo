793ac6c34314200b00327b094d50627863dc04ff
asimha <asimha@qwinixtech.com>
2015-01-20 10:32:06 +0530

    Controller test cases for show action

--- spec/controllers/blogs_controller_spec.rb
+++ spec/controllers/blogs_controller_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe BlogsController, :type => :controller do
 
   describe "POST create" do
     
-    it "user should be able to create blog" do
+    it "user should be able to create and view the blog" do
       blog_params = {
         blogs: {
           title: "My Blog",
@@ -21,6 +21,9 @@ RSpec.describe BlogsController, :type => :controller do
       expect do
         post :create, blog_params
       end.to change(Blog, :count).by(1)
+
+      get :show, id: Blog.first.id
+      expect(assigns[:blog]).to eq(Blog.first)
     end
   
   end
