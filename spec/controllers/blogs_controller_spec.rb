c881c6cd13c4c6670a5814e73090116ac2d1775b
asimha <asimha@qwinixtech.com>
2015-01-20 09:54:24 +0530

    Controller test cases for creating the blog

--- spec/controllers/blogs_controller_spec.rb
+++ spec/controllers/blogs_controller_spec.rb
@@ -1,12 +1,28 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe BlogsController, :type => :controller do
+RSpec.describe BlogsController, :type => :controller do
 
-#   describe "GET index" do
-#     it "returns http success" do
-#       get :index
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
+  describe "GET index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
 
-# end
+  describe "POST create" do
+    
+    it "user should be able to create blog" do
+      blog_params = {
+        blogs: {
+          title: "My Blog",
+          text: "Blog text",
+        }
+      }
+      expect do
+        post :create, blog_params
+      end.to change(Blog, :count).by(1)
+    end
+  
+  end
+
+end
