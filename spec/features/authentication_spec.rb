f065ab187d6c4d61007542bb9fc2e88fc31ff94d
asimha <asimha@qwinixtech.com>
2015-01-22 09:06:33 +0530

    Feature test cases for sign in and log out

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -2,6 +2,7 @@ require_relative '../spec_helper'
 require_relative '../rails_helper'
 
 feature "Sign up" do
+	
 	scenario "User should be able to sign up with proper data" do
 		visit "/users/sign_up"
 		expect(page).to have_text("Sign up")
@@ -12,5 +13,21 @@ feature "Sign up" do
   		expect(page).to have_text("GetInfo")
 	end
 
+end
+
+feature "Sign in and Log out" do
 	
+	let(:user) {FactoryGirl.create(:user)}
+
+	scenario "User should be able to sign in giving valid credentials and log out" do
+		visit "/users/sign_in"
+		expect(page).to have_text("Log in")
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+    expect(page).to have_text("GetInfo")
+    click_button "Log out"
+    expect(page).to have_text("Log in")
+	end
+
 end
