8eea75b6434c7a634acfb72a7bab97bc1f73b167
asimha <asimha@qwinixtech.com>
2015-01-22 14:45:05 +0530

    Read more button added for the post displayed on the home page

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ group :development, :test do
   gem 'rspec-rails'
   gem 'capybara'
   gem 'factory_girl_rails', '~> 4.0'
+  gem 'launchy'
 
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -11,7 +11,7 @@ feature "The user should be able to read and create the blog" do
     expect(page).to have_text("GetInfo")
   end
 
-  scenario "User should be able to create blog" do
+  scenario "User should be able to create and view blog" do
     capybara_sign_in
     expect(page).to have_text("GetInfo")
     click_button "Create Blog"
@@ -22,6 +22,24 @@ feature "The user should be able to read and create the blog" do
   	click_button "Save Blogs"
     expect(page).to have_text("My Blog")
     expect(page).to have_text("Blog Area")
+  end
+
+end
+
+feature "View the complete blog" do
+
+  let(:user) {FactoryGirl.create(:user)}
+  let(:blog) {FactoryGirl.create(:blog, title: "New Blog", text: "loriem ipsum loriem ipsum sdfsdf sdkjfsdf; sldfhsdlfhj sd;fjlhjsdlfg sdjfsdfg kgsdf;sdgf loriem ipsum", user_id: user.id)}
+
+  scenario "User should be able to view complete blog details" do
+    [blog]
+    capybara_sign_in
+    visit "/"
+    expect(page).to have_text("New Blog")
+    expect(page).to have_text("loriem ipsum")
+    click_button "Read More"
+    expect(page).to have_text("New Blog")
+    expect(page).to have_text("loriem ipsum loriem ipsum sdfsdf sdkjfsdf; sldfhsdlfhj sd;fjlhjsdlfg sdjfsdfg kgsdf;sdgf loriem ipsum")
   end
 
 end
